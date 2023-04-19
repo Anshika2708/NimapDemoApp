8ab5748325538378b3a465201bdd5c1b1984fec1
Anshika Singh <singhanshika1831@gmail.com>
2023-04-19 11:13:29 +0530

    latest demo app for nimap

---
+++ src/app/services/register.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+@Injectable({
+  providedIn: 'root'
+})
+export class RegisterService {
+
+  constructor(private http:HttpClient) { }
+  userregister(data:any){
+    return this.http.post('http://localhost:3000/home',data)
+  }
+}
